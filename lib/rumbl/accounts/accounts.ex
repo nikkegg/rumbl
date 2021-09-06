795edbbc24a597a8ab0235c97c9e61bda9109c7e
Nikita Visencuk Visenko <cereshnia@gmail.com>
2021-09-06 13:54:48 +0100

    adds simple views

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -1,8 +1,8 @@
 defmodule Rumbl.Accounts do
-@moduledoc """
-The accounts context
-"""
-alias Rumbl.Accounts.User
+  @moduledoc """
+  The accounts context
+  """
+  alias Rumbl.Accounts.User
 
   def list_users do
     [
@@ -11,11 +11,14 @@ alias Rumbl.Accounts.User
       %User{id: "3", name: "Chris", username: "chrismccord"}
     ]
   end
+
   def get_user(id) do
     Enum.find(list_users(), fn u -> u.id == id end)
   end
+
   def get_user_by(params) do
     Enum.find(list_users(), fn map ->
-    Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end) end)
-    end
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
 end
