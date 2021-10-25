b29a2ab13965341cd6d8986599ad0b2dd42ddc61
Nikita Visencuk Visenko <cereshnia@gmail.com>
2021-10-25 07:41:41 +0300

    authentication

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -2,6 +2,7 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
+  plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
     users = Accounts.list_users()
@@ -21,10 +22,22 @@ defmodule RumblWeb.UserController do
   def create(conn, %{"user" => user_params}) do
     with {:ok, user} <- Accounts.register_user(user_params) do
       conn
+      |> RumblWeb.Auth.login(user)
       |> put_flash(:info, "#{user.name} created!")
       |> redirect(to: Routes.user_path(conn, :index))
     else
       {:error, %Ecto.Changeset{} = changeset} -> render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
   end
 end
